bbbcd8be60039d76fd555a027430c76262146c71
aymenthedeveloper <aymendev.contact@gmail.com>
2024-09-09 23:54:49 +0100

    added count up animation and worked more on the cards layout

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
 const toggleBtn = document.getElementById('toggle-input');
-const cardsContainer = document.querySelector('main .pricing-cards');
+const priceContainers = document.querySelectorAll('main .pricing-cards .card .price');
 
 toggleBtn.addEventListener('click', (e)=> {
-  if (e.target.checked){
-    cardsContainer.classList.add('annually')
-  } else{
-    cardsContainer.classList.remove('annually')
-  }
   toggleBtn.classList.toggle('annually')
+  
+  for (let i = 0; i < 3; i++) {
+    const priceNode = priceContainers[i];
+    const {from, to, plan} = priceNode.dataset;
+    if (plan == 'monthly'){
+      countUp(priceNode, +from, +to, 2)
+      priceNode.dataset.plan = 'annually';
+    } else if (plan == 'annually'){
+      countUp(priceNode, +from, +to, 2, 'down')
+      priceNode.dataset.plan = 'monthly';
+    }
+  }
 })
+
+function countUp(target, from, to, time, direction="up") {
+  if (time <= 0) return 'error';
+  time = time * 1000
+  const diff = to - from;
+  const startTime = performance.now(); // Use high-resolution timestamp
+  function animate(currentTime) {
+    const elapsed = currentTime - startTime; // Time elapsed since animation started
+    const progress = Math.min(elapsed / time, 1); // Ensure progress doesn't exceed 1
+    const easeOut = 1 - (1 - progress)**3; // Ease-in effect
+    const value = direction == 'up'? from + diff * easeOut: to - diff * easeOut;
+    target.firstChild.textContent = value.toFixed(2); 
+    if (progress < 1) {
+      requestAnimationFrame(animate); // Continue the animation
+    }
+  }
+  requestAnimationFrame(animate); // Start the animation
+}
